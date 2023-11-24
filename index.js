8b701d6556680beb5ee6ad5c848209a696520e1c
marcinmika <marcinmika@o2.pl>
2023-11-24 10:31:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require("express");
-
+const mysql = require('mysql2');
 const app = express();
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'test'
+});
+
+function dodaj(obj) {
+
+}
+
+function aktaualizuj(id,obj) {
+
+}
+
+function usun(id){
+
+}
+
+function pobierz(parametr) {
+
+}
+
+
+
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send(data());
 });
 
 app.listen(8080);
